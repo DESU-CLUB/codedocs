fee37ff80b2020949e83d13506056fd23e3b7e2c
DESU-CLUB <warrenlowkh@gmail.com>
2024-06-08 21:37:30 +0800

    added code

--- documentation-app/src/fetcher.js
+++ documentation-app/src/fetcher.js
@@ -1,26 +1,30 @@
 import axios from 'axios';
 
 function downloadNotebook(urlToPost) {
-    axios({
-        url: 'http://localhost:5000/create_notebook', // Adjust the URL based on your Flask app's URL
-        method: 'POST',
-        responseType: 'blob',  // Important: specifies that the response should be treated as a Blob
-        data: {
+    axios.post('http://localhost:5000/create_notebook', // The URL where the POST request is sent
+        {
             url: urlToPost  // JSON object containing the URL you want to post
         },
-        headers: {
-            'Content-Type': 'application/json'
+        {
+            responseType: 'blob',  // Important: specifies that the response should be treated as a Blob
+            headers: {
+                'Content-Type': 'application/json'
+            }
         }
-    }).then((response) => {
+    ).then((response) => {
+        // Create a URL for the blob
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', 'example_notebook.ipynb'); //or any other extension
+        link.setAttribute('download', 'example_notebook.ipynb'); // Set the download attribute to a default filename
         document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-        window.URL.revokeObjectURL(url);
-    }).catch(error => console.error('Download error:', error));
+        link.click(); // Programmatically trigger the link to download
+        link.parentNode.removeChild(link); // Remove the link from the DOM
+        window.URL.revokeObjectURL(url); // Clean up the URL object
+    }).catch(error => {
+        console.error('Download error:', error);
+        alert('Download failed, please try again.');
+    });
 }
 
 export default downloadNotebook;
